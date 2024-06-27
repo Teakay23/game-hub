42d4406da884ddc6dec6c6e59c6ed532d0fb5b5d
teakay23 <thekarachite23@gmail.com>
2024-06-28 03:07:24 +0500

    BugFix: changed game grid error text color.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,7 +13,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text color="red">{error}</Text>;
 
   return (
     <SimpleGrid
