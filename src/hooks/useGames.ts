11a2930290e7a56a953a3a78290eb3453c0accda
teakay23 <thekarachite23@gmail.com>
2024-07-06 06:20:47 +0500

    Refactored fetch games, platforms and genres hooks to use React Query.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,11 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import apiClient, { FetchResponse } from "../services/api-client";
+import { Platform } from "./usePlatforms";
 
 export interface Game {
   id: number;
@@ -17,17 +13,32 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  );
+  useQuery<Game[], Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data.results),
+    staleTime: 1 * 60 * 1000, // 1 minute
+  });
+// useData<Game>(
+//   "/games",
+//   {
+//     params: {
+//       genres: gameQuery.genre?.id,
+//       platforms: gameQuery.platform?.id,
+//       ordering: gameQuery.sortOrder,
+//       search: gameQuery.searchText,
+//     },
+//   },
+//   [gameQuery]
+// );
 
 export default useGames;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient, { FetchResponse } from "../services/api-client";
 
-const usePlatforms = () => ({ data: platforms, error: null });
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+const usePlatforms = () =>
+  useQuery<Platform[], Error>({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data.results),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: platforms,
+  });
 
 export default usePlatforms;
